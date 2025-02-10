b128daf1c80bff7a060c99129f24d454f4177659
Abhay <abhay.bdmu@gmail.com>
2025-02-10 12:48:33 +0530

    update

---
+++ Functions/lambda.py
@@ -0,0 +1,8 @@
+#lamda function syntax=> lambda arguments: expression
+addition = lambda a,b: a+b
+print(addition(10,20))
+
+#passing multiple parameters values
+addition1 = lambda x,y,z: x+y+z
+print(addition1(10,20,30))
+
