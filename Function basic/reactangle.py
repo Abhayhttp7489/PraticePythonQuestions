b222bb31dce256a7bccdc39bf299a396464e1d88
Abhay <abhay.bdmu@gmail.com>
2025-02-16 14:14:08 +0530

    update

---
+++ Function basic/reactangle.py
@@ -0,0 +1,15 @@
+def area_of_rectangle(length, breadth):
+    """
+    Function to calculate the area of a rectangle.
+
+    Parameters:
+    length (float): The length of the rectangle.
+    breadth (float): The breadth of the rectangle.
+
+    Returns:
+    float: The area of the rectangle.
+    """
+    # Calculate the area using the provided parameters
+    area = length * breadth
+    return area
+print(area_of_rectangle(5, 3))
